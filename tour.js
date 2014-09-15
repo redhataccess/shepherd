0f63cf27a07599df4487a60faa5306ebd866c70f
Connor Hindley <conn.hindley@gmail.com>
2014-09-14 20:34:45 -0400

    initial publish

---
+++ tour.js
@@ -0,0 +1,24 @@
+'use strict';
+var paths = @@paths;
+
+function getCurrentSteps() {
+    var path = window.location.pathname;
+
+    if (paths[path]) {
+        return paths[path];
+    }
+    for (path in paths) {
+        return paths[path];
+    }
+}
+
+function startIntro() {
+    var intro = introJs();
+
+    intro.setOptions({
+        steps: getCurrentSteps()
+    });
+
+    intro.start();
+}
+startIntro();
