e1bf1ab261344b38922cef8504c914271df63190
Connor Hindley <conn.hindley@gmail.com>
2014-09-15 18:12:05 -0400

    add deps to build step

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,29 @@
 var gulp = require('gulp');
 var jsoncombine = require('gulp-jsoncombine');
 var replace = require('gulp-replace-task');
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
+
+gulp.task('copy-deps', function() {
+    gulp.src('bower_components/intro.js/introjs.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist'));
+
+    gulp.src('bower_components/intro.js/intro.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
 
 gulp.task('inject', function() {
     function inject(paths) {
         gulp.src('./tour.js')
-        .pipe(replace({
-            patterns: [{
-                match: 'paths',
-                replacement: paths
-            }]
-        }))
-        .pipe(gulp.dest('./dist'));
+            .pipe(replace({
+                patterns: [{
+                    match: 'paths',
+                    replacement: paths
+                }]
+            }))
+            .pipe(gulp.dest('./dist'));
     }
 
     gulp.src('./paths/**/*.json')
@@ -21,4 +33,4 @@ gulp.task('inject', function() {
 
 });
 
-gulp.task('default', ['inject']);
+gulp.task('default', ['copy-deps', 'inject']);
