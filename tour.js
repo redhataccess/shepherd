ae8958ffd98ec688103cc053016fbc581bd538fc
Connor Hindley <conn.hindley@gmail.com>
2014-09-16 15:31:43 -0400

    publish: change cdn
    generated from commit da27ebf5ab13d1a22cf5b5258933e7ea7d3c5dfa

--- tour.js
+++ tour.js
@@ -1,5 +1,5 @@
 (function(){window.tours = {"home":{"steps":[{"intro":"Welcome to the customer portal!"},{"element":"#navhome","intro":"This will always bring you back home"},{"element":"#navproducts","intro":"Here are all the cool products"},{"element":"#navsupport","intro":"Here is all the cool support stuff"},{"element":"#navdownloads","intro":"Here is all the cool download stuff"},{"element":"#navcertification","intro":"If you want to be cool and get certified"},{"element":"#navsecurity","intro":"Security Information"},{"element":"#navsubscription","intro":"Sub"}]}};
-require(['//cdnjs.cloudflare.com/ajax/libs/intro.js/0.5.0/intro.min.js', 'jquery'], function(introJs, $) {
+require(['//cdn.jsdelivr.net/intro.js/0.9.0/intro.min.js', 'jquery'], function(introJs, $) {
     var paths = window.tours;
     function getCurrentSteps() {
         var path = window.location.pathname;
