eacbb34f5446e36174ef2a291bcc588ac1f8517c
Connor Hindley <conn.hindley@gmail.com>
2014-09-17 14:27:50 -0400

    add some callbacks

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
           footer: '}());',
         },
         files: {
-          'dist/tour.js': ['.tmp/tours.js', 'tour.js']
+          'dist/tour.js': ['actions.js', '.tmp/tours.js', 'tour.js']
         }
       }
     },
--- actions.js
+++ actions.js
@@ -1,3 +1,8 @@
 var actions = {
-
+    doThisOneBefore: function() {
+        console.log('action before');
+    },
+    doThisOneAfter: function() {
+        console.log('action after');
+    }
 };
