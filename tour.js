f51d6f2df64e3082b15b88186b34b0dc3bbc118e
Connor Hindley <conn.hindley@gmail.com>
2014-09-16 20:47:40 -0400

    update tour

--- tour.js
+++ tour.js
@@ -1,17 +1,21 @@
 require(['//cdn.jsdelivr.net/intro.js/0.9.0/intro.min.js'], function(introJs) {
-    var paths = window.tours;
+    var tours = window.tours;
+
     function getCurrentSteps() {
         var path = window.location.pathname;
-
-        if (paths[path]) {
-            return paths[path].steps;
-        }
-        for (path in paths) {
-            return paths[path].steps;
+        for (var tour in tours) {
+            if (new RegExp(tour).test(path)) {
+                return tours[tour].steps;
+            }
         }
     }
 
     function startIntro() {
+        var steps = getCurrentSteps();
+        if (!steps) {
+            // Couldn't find tour, bail.
+            return;
+        }
         var intro = introJs();
         intro.setOptions({
             steps: getCurrentSteps()
