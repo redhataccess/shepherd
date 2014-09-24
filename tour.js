daed2b8747ee54c3f5eaa721d39e3ce012a82283
Connor Hindley <conn.hindley@gmail.com>
2014-09-24 08:46:43 -0400

    publish: add class to body while tour is running
    generated from commit 24526dab96aca1bbbb0658654e4c2e2ecb4b0aae

--- tour.js
+++ tour.js
@@ -83,6 +83,9 @@ PortalTour.prototype.buildTour = function() {
             }
         }
     };
+    var onFinish = function() {
+        jQuery('body').removeClass('portal-tour');
+    };
     this.intro.onbeforechange(function(element) {
         self.actions.resetMega();
         this.executeCurrentStepCb('before');
@@ -93,9 +96,12 @@ PortalTour.prototype.buildTour = function() {
     this.intro.onafterchange(function(element) {
         this.executeCurrentStepCb('after');
     });
+    this.intro.oncomplete(onFinish);
+    this.intro.onexit(onFinish);
 };
 
 PortalTour.prototype.startTour = function() {
+    jQuery('body').addClass('portal-tour');
     this.intro.start();
     this.hijackClicks();
 };
