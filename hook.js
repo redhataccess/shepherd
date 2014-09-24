c90252a6a1fcd939570bd17900cccbd99196a67a
Connor Hindley <conn.hindley@gmail.com>
2014-09-24 16:39:56 -0400

    add hook

---
+++ hook.js
@@ -0,0 +1,1 @@
+javascript:(function(e){"use strict";var n=e.createElement("link");n.rel="stylesheet";n.href="https://rawgit.com/connyay/access-tours/built/tour.css";e.head.appendChild(n);var r=e.createElement("script");r.setAttribute("src","https://rawgit.com/connyay/access-tours/built/tour.js");e.head.appendChild(r)})(document)
