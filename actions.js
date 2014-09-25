aed2a1197f820d684f8fd2902e15c17c6d791249
Connor Hindley <conn.hindley@gmail.com>
2014-09-25 12:19:26 -0400

    clean up

--- actions.js
+++ actions.js
@@ -26,23 +26,5 @@ var actions = {
     },
     openCommunity: function() {
         utils.click('#nav-community > a');
-    },
-    trySearch: function() {
-        var searchQuery = 'Red Hat Satellite',
-            $searchForm = jQuery('#homePageSearchForm'),
-            $searchInput = jQuery('#search-query');
-        $searchInput.attr('placeholder', '');
-        $searchForm.attr('action', $searchForm.attr('action') + '?tour=true');
-        $searchInput.typed({
-            strings: [searchQuery],
-            typeSpeed: 42,
-            attr: 'value',
-            showCursor: false,
-            callback: function() {
-                setTimeout(function() {
-                    $searchForm.submit();
-                }, 400);
-            }
-        });
     }
 };
