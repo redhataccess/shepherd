2dc75e2e1f6d986193fffa16d10084678dc9079a
Connor Hindley <conn.hindley@gmail.com>
2014-09-25 08:46:15 -0400

    publish: abstract tour callbacks
    generated from commit 6b35a1ef992ef92f3916ecf8dabb06ef66c5fad8

--- tour.js
+++ tour.js
@@ -49,7 +49,7 @@ var actions = {
     }
 };
 
-var tours = {"/home/?$":{"steps":[{"intro":"<h1>Welcome to the newly redesigned Customer Portal</h1><h2>Tour the new layout to see what's changed</h2>","tooltipClass":"tooltip-lg"},{"element":".primary-nav","intro":"We've refined our navigation to make it easier to find things in the Portal","tooltipClass":"tooltip-md"},{"element":".products-menu .col-md-6.col-sm-4.pull-right","intro":"Products & Services is the single place to find everything you need about your product. Certification, Product documentation and content, and Support Policies can all be found from here.","tooltipClass":"tooltip-md","on":"openProducts","position":"left"},{"element":".tools-menu .col-sm-9.basic","intro":"Tools is a new place for everything Red Hat Support develops to make your life easier. Red Hat Access Labs and Plug-in, and additional tools can now all be found here.","tooltipClass":"tooltip-md","before":"openTools"},{"element":".security-menu .col-sm-12.basic","intro":"Everything Security related can be found here including the CVE database, information on the Red Hat Product Security team, and relevant policy information.","tooltipClass":"tooltip-md","before":"openSecurity"},{"element":".community-menu .col-sm-12.basic","intro":"Community is now the place for our two-way interactions! Discussions, Blogs, Events, and feedback are now available from here.","tooltipClass":"tooltip-md","before":"openCommunity"},{"element":".top-nav ul","intro":"We've placed important utilities at the top of the Customer Portal. Now, you're only one click away any time from managing your subscriptions, downloads, or support cases.","tooltipClass":"tooltip-md","position":"right","highlightClass":"light top"},{"element":".utility-nav ul","intro":"You can search, login, and change your language from this global bar at any time!","tooltipClass":"tooltip-md","position":"left","highlightClass":"light top right"},{"element":".home-quick-links","intro":"We've also provided some key tasks from here to jump right into the tasks that matter the most to you.","tooltipClass":"tooltip-md","position":"top","highlightClass":"light"},{"element":".home-bottom .row","intro":"See what's new in the Customer Portal including important announcements, new labs, product releases, and more.","tooltipClass":"tooltip-md","position":"top","highlightClass":"light"}]}};
+var tours = {"/home/?$":{"steps":[{"intro":"<h1>Welcome to the newly redesigned Customer Portal</h1><h2>Tour the new layout to see what's changed</h2>","tooltipClass":"tooltip-lg"},{"element":".primary-nav","intro":"We've refined our navigation to make it easier to find things in the Portal","tooltipClass":"tooltip-md"},{"element":".products-menu .col-md-6.col-sm-4.pull-right","intro":"Products & Services is the single place to find everything you need about your product. Certification, Product documentation and content, and Support Policies can all be found from here.","tooltipClass":"tooltip-md","on":"openProducts","position":"left"},{"element":".tools-menu .col-sm-9.basic","intro":"Tools is a new place for everything Red Hat Support develops to make your life easier. Red Hat Access Labs and Plug-in, and additional tools can now all be found here.","tooltipClass":"tooltip-md","before":"openTools"},{"element":".security-menu .col-sm-12.basic","intro":"Everything Security related can be found here including the CVE database, information on the Red Hat Product Security team, and relevant policy information.","tooltipClass":"tooltip-md","before":"openSecurity"},{"element":".community-menu .col-sm-12.basic","intro":"Community is now the place for our two-way interactions! Discussions, Blogs, Events, and feedback are now available from here.","tooltipClass":"tooltip-md","before":"openCommunity"},{"element":".top-nav ul","intro":"We've placed important utilities at the top of the Customer Portal. Now, you're only one click away any time from managing your subscriptions, downloads, or support cases.","tooltipClass":"tooltip-md","position":"right","highlightClass":"light top"},{"element":".utility-nav ul","intro":"You can search, login, and change your language from this global bar at any time!","tooltipClass":"tooltip-md","position":"left","highlightClass":"light top right"},{"element":".home-quick-links","intro":"We've also provided some key tasks from here to jump right into the tasks that matter the most to you.","tooltipClass":"tooltip-md","position":"top","highlightClass":"light"},{"element":".home-bottom .row","intro":"See what's new in the Customer Portal including important announcements, new labs, product releases, and more.","tooltipClass":"tooltip-md","position":"top","highlightClass":"light"}]},"callBacks":{"before":"resetMega"}};
 'use strict';
 var PortalTour = function(introjs) {
     this.intro = introjs();
@@ -59,6 +59,7 @@ var PortalTour = function(introjs) {
 PortalTour.prototype.init = function(tours, actions) {
     this.tours = tours;
     this.actions = actions;
+    this.tourCallbacks = {};
 
     this.currentSteps = this.getCurrentSteps();
     this.buildTour();
@@ -75,7 +76,9 @@ PortalTour.prototype.getCurrentSteps = function() {
     var path = window.location.pathname;
     for (var tour in this.tours) {
         if (new RegExp(tour).test(path)) {
-            return this.tours[tour].steps;
+            var current = this.tours[tour];
+            this.tourCallbacks = current.callBacks || {};
+            return current.steps;
         }
     }
 };
@@ -87,6 +90,9 @@ PortalTour.prototype.buildTour = function() {
         buttonClass: 'btn btn-sm',
     });
     this.intro.executeCurrentStepCb = function(phase) {
+        if (self.tourCallbacks[phase] && self.actions[self.tourCallbacks[phase]]) {
+            self.actions[self.tourCallbacks[phase]]();
+        }
         if (this._options && this._options.steps && this._currentStep) {
             var step = this._options.steps[this._currentStep];
             if (step && step[phase] && self.actions[step[phase]]) {
@@ -98,7 +104,6 @@ PortalTour.prototype.buildTour = function() {
         jQuery('body').removeClass('portal-tour');
     };
     this.intro.onbeforechange(function(element) {
-        self.actions.resetMega();
         this.executeCurrentStepCb('before');
     });
     this.intro.onchange(function(element) {
