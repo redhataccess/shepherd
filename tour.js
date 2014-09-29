b7a3b8f2b8a1eeec1a421b20790bbe3327ce0ba7
Connor Hindley <conn.hindley@gmail.com>
2014-09-29 19:26:57 -0400

    publish: remove uglify
    generated from commit 14dcd019e1d82c93537682dc575ae02b3aa02953

--- tour.js
+++ tour.js
@@ -1,4 +1,5 @@
-define(['introjs'], function (introjs) {var actions = {
+define(['introjs'], function (introjs) {
+var actions = {
     _: {
         click: function(selector) {
             var elem = document.querySelector(selector);
@@ -31,25 +32,37 @@ define(['introjs'], function (introjs) {var actions = {
 var tours = {"/home/?$":{"steps":[{"intro":"<h1>Welcome to the newly redesigned Customer Portal</h1><h2>Tour the new layout to see what's changed</h2>","tooltipClass":"tooltip-lg"},{"element":".primary-nav","intro":"We've refined our navigation to make it easier to find things in the Portal","tooltipClass":"tooltip-md"},{"element":".products-menu .col-md-6.col-sm-4.pull-right","intro":"<strong>Products & Services</strong> is the single place to find everything you need about your product. Certification, Product documentation and content, and Support Policies can all be found from here.","tooltipClass":"tooltip-md","on":"openProducts","position":"left"},{"element":".tools-menu .col-sm-9.basic","intro":"<strong>Tools</strong> is a new place for everything Red Hat Support develops to make your life easier. Red Hat Access Labs and Plug-in, and additional tools can now all be found here.","tooltipClass":"tooltip-md","before":"openTools"},{"element":".security-menu .col-sm-12.basic","intro":"Everything <strong>Security</strong> related can be found here including the CVE database, information on the Red Hat Product Security team, and relevant policy information.","tooltipClass":"tooltip-md","before":"openSecurity"},{"element":".community-menu .col-sm-12.basic","intro":"<strong>Community</strong> is now the place for our two-way interactions! Discussions, Blogs, Events, and feedback are now available from here.","tooltipClass":"tooltip-md","before":"openCommunity"},{"element":".top-nav ul","intro":"We've placed important utilities at the top of the Customer Portal. Now, you're only one click away any time from managing your subscriptions, downloads, or support cases.","tooltipClass":"tooltip-md","position":"right","highlightClass":"light top"},{"element":".utility-nav ul","intro":"You can search, login, and change your language from this global bar at any time!","tooltipClass":"tooltip-md","position":"left","highlightClass":"light top right"},{"element":".home-quick-links","intro":"We've also provided some key tasks from here to jump right into the tasks that matter the most to you.","tooltipClass":"tooltip-md","position":"top","highlightClass":"light"},{"element":".home-bottom .row","intro":"See what's new in the Customer Portal including important announcements, new labs, product releases, and more.","tooltipClass":"tooltip-md","position":"top","highlightClass":"light"}],"callBacks":{"before":"resetMega"}}};
 'use strict';
 
-var hasClass = function (elem, className) {
+var hasClass = function(elem, className) {
     return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-}
-
-var addClass = function (elem, className) {
+};
+var addClass = function(elem, className) {
     if (!hasClass(elem, className)) {
         elem.className += ' ' + className;
     }
-}
-
-var removeClass = function (elem, className) {
-    var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+};
+var removeClass = function(elem, className) {
+    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
     if (hasClass(elem, className)) {
-        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0) {
             newClass = newClass.replace(' ' + className + ' ', ' ');
         }
         elem.className = newClass.replace(/^\s+|\s+$/g, '');
     }
-}
+};
+var searchToObject = function() {
+    if (!location.search) {
+        return {};
+    }
+    var result = {},
+        pairs = location.search.slice(1).split('&'),
+        length = pairs.length;
+    for (var i = 0; i < length; i++) {
+        var pair = pairs[i];
+        pair = pair.split('=');
+        result[pair[0]] = decodeURIComponent(pair[1] || '');
+    }
+    return result;
+};
 
 var PortalTour = function() {
     this.intro = introjs();
@@ -64,7 +77,7 @@ PortalTour.prototype.init = function(tours, actions) {
     this.currentSteps = this.getCurrentSteps();
     this.buildTour();
 
-    var searchObj = this.utils.searchToObject();
+    var searchObj = searchToObject();
     if (this.currentSteps && searchObj.tour) {
         this.startTour();
     }
@@ -126,23 +139,6 @@ PortalTour.prototype.startTour = function() {
     window.scrollTo(0, 0);
     addClass(document.body, 'portal-tour');
     this.intro.start();
-};
-
-PortalTour.prototype.utils = {};
-
-PortalTour.prototype.utils.searchToObject = function() {
-    if (!location.search) {
-        return {};
-    }
-    var result = {},
-        pairs = location.search.slice(1).split('&'),
-        length = pairs.length;
-    for (var i = 0; i < length; i++) {
-        var pair = pairs[i];
-        pair = pair.split('=');
-        result[pair[0]] = decodeURIComponent(pair[1] || '');
-    }
-    return result;
 };
 
 return PortalTour;
