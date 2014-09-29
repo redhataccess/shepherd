f0fba221e9ca24fb0129a9270d63760c4c137a30
Connor Hindley <conn.hindley@gmail.com>
2014-09-29 14:02:17 -0400

    ._.

--- actions.js
+++ actions.js
@@ -1,30 +1,29 @@
-var utils = {
-    click: function(selector) {
-        var elem = document.querySelector(selector);
-        elem && elem.click && elem.click();
-    }
-};
-
 var actions = {
+    _: {
+        click: function(selector) {
+            var elem = document.querySelector(selector);
+            elem && elem.click && elem.click();
+        }
+    },
     resetMega: function() {
         // Determine if it is active
         var isActive = document.querySelector('.mega.active');
-        if(isActive){
+        if (isActive) {
             // It is active... Find the menu that is open and then close it.
             var activeMenu = document.querySelector('.primary-nav .active > a');
             activeMenu && activeMenu.click && activeMenu.click();
         }
     },
     openProducts: function() {
-        utils.click('#nav-products > a');
+        this._.click('#nav-products > a');
     },
     openTools: function() {
-        utils.click('#nav-tools > a');
+        this._.click('#nav-tools > a');
     },
     openSecurity: function() {
-        utils.click('#nav-security > a');
+        this._.click('#nav-security > a');
     },
     openCommunity: function() {
-        utils.click('#nav-community > a');
+        this._.click('#nav-community > a');
     }
 };
